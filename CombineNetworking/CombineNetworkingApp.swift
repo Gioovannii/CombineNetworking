23ce950add16992591567e2041e140cf5e982fa6
Giovanni Gaffe <gaffejonathan@ymail.com>
2022-02-21 10:50:52 +0800

    Initial Commit

---
+++ CombineNetworking/CombineNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkingApp.swift
+//  CombineNetworking
+//
+//  Created by Giovanni Gaffé on 2022/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
